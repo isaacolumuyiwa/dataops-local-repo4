d7faa14e1187ce7d56134f9811fb2048dc093019
Isaac Olumuyiwa <isaacolumuyiwa@gmail.com>
2022-02-10 23:01:01 +0100

    File update
    Adding React source code

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import logo1 from './logo1.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+	<img src={logo1} className="App-logo" alt="logo1" />
+        <p>
+         I Just edited this text in my app!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
